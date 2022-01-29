b617c0dcbd509a125a6d6c50ccd6e0eb656c12ca
Beknasar <680633@gmail.com>
2022-01-29 13:45:04 +0600

    Добавлен вывод постов на главную страницу Созданы частичные шаблоны и шаблон главной страницы

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.db.models import Q
+from webapp.models import Post
+from django.views.generic import ListView
+from webapp.forms import SearchForm
 
-# Create your views here.
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'posts'
+    paginate_by = 7
+    paginate_orphans = 0
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        form = SearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            kwargs['search'] = search
+        kwargs['form'] = form
+        return super().get_context_data(object_list=object_list, **kwargs)
+
+    def get_queryset(self):
+        data = Post.objects.all()
+        form = SearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            if search:
+                data = data.filter(Q(title__icontains=search) | Q(author__icontains=search))
+        return data.order_by('-date_create')
