9ba46610c8e1735c7dafab954bf258ba0b5a21a4
Beknasar <680633@gmail.com>
2022-01-30 10:42:45 +0600

    Добавлено приложение API и пара точек входа.

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-app_name = 'api'
+from api.views import get_token_view, PostCreateView
+
+app_name = 'api_v1'
+
 
 urlpatterns = [
-
+    path('get_token/', get_token_view, name='get_token'),
+    path('post_create/', PostCreateView.as_view(), name="post_create"),
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.views .generic import View
+from django.views.decorators.csrf import ensure_csrf_cookie
+from webapp.models import Post
 
-# Create your views here.
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+class PostCreateView(View):
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        print(data)
+        post = Post.objects.create(
+            author_id=data['author_id'],
+            title=data['title'],
+            link=data['link']
+        )
+        return JsonResponse({
+            'pk': post.pk,
+            'author_id': post.author_id,
+            'title': post.title,
+            'link': post.link,
+            'date_create': post.date_create
+        })
+
