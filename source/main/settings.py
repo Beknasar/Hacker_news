22e3a1cad0ef061950227e1496d30b3f0faf9e57
Beknasar <680633@gmail.com>
2022-01-30 19:03:46 +0600

    Добавлены доп настройки для запуска на локальной машине(при желании закомментировать, а похожие расскоментировать)

--- source/main/settings.py
+++ source/main/settings.py
@@ -20,15 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-print(SECRET_KEY)
-# 'e-6bu!!4x=1&qi0ku)1d-ac6ajb9-s@eejj6l(tzah)u()yofl'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'e-6bu!!4x=1&qi0ku)1d-ac6ajb9-s@eejj6l(tzah)u()yofl'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+# DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = []
 
 # Application definition
 
